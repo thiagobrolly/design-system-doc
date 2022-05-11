9752206457281dba4f0683d1306b4ddd55810e49
Thiago Sousa <thiago.sousa@paylivre.com>
2022-05-11 18:17:55 -0300

    feat(root): initial config

---
+++ packages/components-ui/src/components/Button/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from './styles';
+
+export type ButtonProps = {
+  label: string;
+}
+
+export const Button = ({ label }: ButtonProps) => (
+  <S.Button>
+    {label}
+  </S.Button>
+);
---
+++ packages/components-ui/src/components/Button/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  width: fit-content;
+  padding: 10px;
+  font-weight: bold;
+  text-align: center;
+  border-radius: 5px;
+  text-decoration: none;
+  cursor: pointer;
+`;
