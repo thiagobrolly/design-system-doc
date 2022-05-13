8015446f6b8be67b80c50db993d13370e334b640
Thiago Sousa <thiago.sousa@paylivre.com>
2022-05-13 12:14:03 -0300

    chore(eslint): add Commitlint, Husky, Commitizen, lint-staged, eslint, prettir e editorconfig

--- packages/components-ui/src/components/Button/index.tsx
+++ packages/components-ui/src/components/Button/index.tsx
@@ -3,10 +3,6 @@ import * as S from './styles';
 
 export type ButtonProps = {
   label: string;
-}
+};
 
-export const Button = ({ label }: ButtonProps) => (
-  <S.Button>
-    {label}
-  </S.Button>
-);
+export const Button = ({ label }: ButtonProps) => <S.Button>{label}</S.Button>;
